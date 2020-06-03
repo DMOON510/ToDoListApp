eafe6e0ac4709dba34a01f6fbe203d12af2f67bb
DMOON510 <51987755+DMOON510@users.noreply.github.com>
2020-06-03 15:34:25 -0700

    commit

---
+++ ToDoListApp/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  ToDoListApp
+//
+//  Created by Donna Moon on 6/2/20.
+//  Copyright © 2020 Donna Moon. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
